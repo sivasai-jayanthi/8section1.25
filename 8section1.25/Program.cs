32c36f90a474b34369e3432f527233b0fa4cbf13
sivasai-jayanthi <sai89938@gmail.com>
2023-10-30 00:46:07 +0530

    Add project files.

---
+++ 8section1.25/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _8section1._25
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			runApp();
+			Console.ReadLine();
+		}
+
+		public static void runApp()
+		{
+			string[] students = new string[10];
+			students[0] = "Karuna";
+			students[1] = "Mark";
+			students[2] = "Advaith";
+			students[3] = "Sangeeta";
+			students[4] = "Nazia";
+			students[5] = "Faisal";
+			students[6] = "Tania";
+			students[7] = "Guru";
+			students[8] = "Chandni";
+			students[9] = "Kamleshwar";
+
+			int currElem = 0;
+			printViaRecursion(students, currElem);
+		}
+
+		public static void printViaRecursion(string[] arr, int currElem)
+		{
+			Console.WriteLine(arr[currElem]);
+			if (currElem < arr.Length - 1)
+				printViaRecursion(arr, currElem + 1);
+		}
+	}
+}
